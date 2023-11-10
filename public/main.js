81299f8af2ed68e9c63f6182ff50ff5833e74390
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-11-10 10:00:53 -0500

    Add translate method

--- public/main.js
+++ public/main.js
@@ -10,9 +10,22 @@ class Ball {
     ctx.fillStyle = "#fff";
     ctx.fillRect(this.position.x, this.position.y, 10, 10);
   }
+
+  translate(x, y) {
+    this.position.x += x;
+    this.position.y += y;
+  }
 }
 
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-
 const b = new Ball(10, 10);
+
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 b.draw();
+
+b.translate(100, 100);
+
+setTimeout(() => {
+  ctx.fillStyle = "#000";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  b.draw();
+}, 500);
