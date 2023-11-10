496737fd84edaca89d8f27649016b6e48e5bf96f
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-11-10 09:48:16 -0500

    Add ball

--- public/main.js
+++ public/main.js
@@ -1,4 +1,18 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+class Ball {
+  constructor(x, y) {
+    this.position = { x, y };
+  }
+
+  draw() {
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(this.position.x, this.position.y, 10, 10);
+  }
+}
+
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+const b = new Ball(10, 10);
+b.draw();
